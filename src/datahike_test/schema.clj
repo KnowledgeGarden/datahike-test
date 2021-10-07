6de0d8e17ff762266f2c990d359bc43cb11fb7a0
Jack Park <jackpark@gmail.com>
2021-10-07 11:31:10 -0700

    more schema notes

--- src/datahike_test/schema.clj
+++ src/datahike_test/schema.clj
@@ -12,3 +12,12 @@
 ;; to round things out, we may want to do probabilistic reasoning
 ;;  { id, entity, entityType, attribute, value, valueType, transaction, probability, truth }
 ;; where "truth" is `true` until a tuple is rescinded
+;; NOTE: these ideas are locked into the structure made by Datahike.
+;; Here are some domain schema datoms from early testing in core.clj
+;; #datahike/Datom [1 :db/ident :name 536870913 true]
+;; #datahike/Datom [1 :db/valueType :db.type/string 536870913 true]
+;; #datahike/Datom [1 :db/cardinality :db.cardinality/one 536870913 true]
+;; #datahike/Datom [2 :db/ident :age 536870913 true]
+;; #datahike/Datom [2 :db/valueType :db.type/long 536870913 true]
+;; #datahike/Datom [2 :db/cardinality :db.cardinality/one 536870913 true]]
+
